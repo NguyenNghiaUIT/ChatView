cc0800822c11c63706d109914c2a211789a8d49b
NguyenNghiaUIT <nghiaklpro195@gmail.com>
2016-07-05 18:22:38 +0700

    check null result return from ZaloAvatarManager.getAllFiles

--- app/src/main/java/com/example/nguyennghia/circleimageview/ListViewCircleImageTestActivity.java
+++ app/src/main/java/com/example/nguyennghia/circleimageview/ListViewCircleImageTestActivity.java
@@ -28,28 +28,30 @@ public class ListViewCircleImageTestActivity extends AppCompatActivity {
 
 
         File[] files = ZaloAvatarManager.getAllFiles();
+        if(files != null){
+            mChatInfos = new ArrayList<>();
+            for (int i = 0; i < 200; i++) {
+                Picture p = new Picture();
+                int value = mRandom.nextInt(10);
+                if (value == 0) {
+                    p.setUrl(files[0].getAbsolutePath());
+                } else if (value == 1) {
+                    p.setUrl(files[1].getAbsolutePath(), files[2].getAbsolutePath());
 
-        mChatInfos = new ArrayList<>();
-        for (int i = 0; i < 200; i++) {
-            Picture p = new Picture();
-            int value = mRandom.nextInt(10);
-            if (value == 0) {
-                p.setUrl(files[0].getAbsolutePath());
-            } else if (value == 1) {
-                p.setUrl(files[1].getAbsolutePath(), files[2].getAbsolutePath());
-
-            } else if (value == 2) {
-                p.setUrl(files[3].getAbsolutePath(), files[4].getAbsolutePath(), files[5].getAbsolutePath());
-            } else if (value == 3) {
-                p.setUrl(files[6].getAbsolutePath(), files[7].getAbsolutePath(), files[8].getAbsolutePath(), files[9].getAbsolutePath());
-            } else {
-                p.setUrl(files[10].getAbsolutePath(), files[11].getAbsolutePath(), files[12].getAbsolutePath(), files[13].getAbsolutePath(), files[14].getAbsolutePath());
+                } else if (value == 2) {
+                    p.setUrl(files[3].getAbsolutePath(), files[4].getAbsolutePath(), files[5].getAbsolutePath());
+                } else if (value == 3) {
+                    p.setUrl(files[6].getAbsolutePath(), files[7].getAbsolutePath(), files[8].getAbsolutePath(), files[9].getAbsolutePath());
+                } else {
+                    p.setUrl(files[10].getAbsolutePath(), files[11].getAbsolutePath(), files[12].getAbsolutePath(), files[13].getAbsolutePath(), files[14].getAbsolutePath());
+                }
+                mChatInfos.add(new ChatInfo("This is Title" + i, "This is content " + i, i + " minutes", p));
             }
-            mChatInfos.add(new ChatInfo("This is Title" + i, "This is content " + i, i + " minutes", p));
+            mAdapter = new ChatViewAdapter(this, mChatInfos);
+            lvAuthor.setAdapter(mAdapter);
+
         }
 
-        mAdapter = new ChatViewAdapter(this, mChatInfos);
-        lvAuthor.setAdapter(mAdapter);
 
 
     }
